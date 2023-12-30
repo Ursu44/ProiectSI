0508337c1b7c72b54fc8d6fe49aca7caafe3f8c1
Ursu44 <147915071+Ursu44@users.noreply.github.com>
2023-12-30 14:46:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import random
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import bluetooth
 from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import bluetooth
 
 app = Flask(__name__, template_folder='C:\\Users\\User\\Desktop\\ProiectSI\\templetates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -16,6 +17,8 @@ port = 1
 sock = None
 
 dynamic_value = 0
+last_email_sent = None
+in_interval = True
 
 def establish_bluetooth_connection():
     global sock
@@ -37,6 +40,20 @@ def send_mail(mailTo, value, subject):
     message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
 
 
+data = {
+    'Temperatura': [18, 22, 25, 30, 35, 19, 21, 33],
+    'Avertizare': [0, 0, 0, 1, 1, 0, 0, 1]
+}
+
+df = pd.DataFrame(data, columns=['Temperatura', 'Avertizare'])
+
+X_train = df[['Temperatura']]
+y_train = df['Avertizare']
+
+# Antrenare model RandomForest
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
 @app.route('/')
 def index():
     return render_template('index.html', dynamic_value=dynamic_value)
@@ -46,7 +63,7 @@ def handle_connect():
     print('Client connected')
     emit('update_dynamic_value', {'dynamic_value': dynamic_value})
 
-email_to = None  # Variabila globala pentru adresa de e-mail
+email_to = None
 
 @socketio.on('update_email')
 def handle_update_email(data):
@@ -55,7 +72,7 @@ def handle_update_email(data):
 
 @socketio.on('request_dynamic_value')
 def handle_request_dynamic_value():
-    global dynamic_value
+    global dynamic_value, last_email_sent, in_interval, email_sent
     new_dynamic_value = 0
     dynamic_value = new_dynamic_value
     if sock is None:
@@ -66,13 +83,27 @@ def handle_request_dynamic_value():
     if data_received:
         dynamic_value = float(values[0])
         print(f"Received dynamic value from Bluetooth: {dynamic_value}")
-    if new_dynamic_value > 32 and email_to:
-        print(new_dynamic_value)
-        send_mail(email_to, f': Atentie,este prea cald in casa')
 
-    elif new_dynamic_value < 19 and email_to:
-        send_mail(email_to, f': Atentie,este prea frig in casa')
+        # Atribuire implicită pentru in_interval și email_sent
+        in_interval = in_interval if 'in_interval' in locals() else True
+        email_sent = email_sent if 'email_sent' in locals() else False
 
+        if not in_interval:
+            # Verificăm dacă temperatura a revenit în interval
+            if 19 <= dynamic_value <= 32:
+                in_interval = True
+                email_sent = False
+
+        if in_interval and not email_sent:
+            if dynamic_value > 32 and email_to:
+                print(dynamic_value)
+                send_mail(email_to, 'Depasire temperatura', f'Atentie este prea cald in casa')
+                last_email_sent = 'cool'
+                email_sent = True
+            elif dynamic_value < 19 and email_to:
+                send_mail(email_to, 'Depasire temperatur', f'Atentie este prea cald in casa')
+                last_email_sent = 'warm'
+                email_sent = True
 
     emit('update_dynamic_value', {'dynamic_value': dynamic_value}, broadcast=True)
 
@@ -80,8 +111,7 @@ def handle_request_dynamic_value():
 @socketio.on('send_current_temperature')
 def handle_send_current_temperature():
     if email_to:
-        send_mail(email_to, f'Temperatura actuala: {dynamic_value}')
-
+        send_mail(email_to, f'Temperatura actuala este de: {dynamic_value} grade Celsius', ': Actualizare temperatura')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5002)
