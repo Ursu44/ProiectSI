873d0cc68805cc07c96379fc5c26190070ee22b7
Ursu44 <147915071+Ursu44@users.noreply.github.com>
2023-12-30 14:06:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def establish_bluetooth_connection():
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((server_address, port))
 
-
-def send_mail(mailTo, value):
+def send_mail(mailTo, value, subject):
     CLIENT_SECRET_FILE = 'credentials.json'
     API_NAME = 'gmail'
     API_VERSION = 'v1'
@@ -32,7 +31,7 @@ def send_mail(mailTo, value):
     emailMsg = value
     mimeMessage = MIMEMultipart()
     mimeMessage['to'] = mailTo
-    mimeMessage['subject'] = 'Automated email'
+    mimeMessage['subject'] = subject
     mimeMessage.attach(MIMEText(emailMsg, 'plain', 'utf-8'))
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
     message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
@@ -53,30 +52,35 @@ email_to = None  # Variabila globala pentru adresa de e-mail
 def handle_update_email(data):
     global email_to
     email_to = data['email']
-    print('Setat')
 
 @socketio.on('request_dynamic_value')
 def handle_request_dynamic_value():
     global dynamic_value
-    new_dynamic_value = random.randint(1, 100)
+    new_dynamic_value = 0
     dynamic_value = new_dynamic_value
-    print('da')
-    print(email_to)
-    print(new_dynamic_value)
-    if new_dynamic_value > 80 and email_to:
-        print(new_dynamic_value)
-        print('da1')
-        send_mail(email_to, f'Test email. Dynamic value: {new_dynamic_value}')
+    if sock is None:
+        establish_bluetooth_connection()
 
-    emit('update_dynamic_value', {'dynamic_value': new_dynamic_value}, broadcast=True)
+    data_received = sock.recv(1024).decode().strip()
+    values = data_received.split('\n')
+    if data_received:
+        dynamic_value = float(values[0])
+        print(f"Received dynamic value from Bluetooth: {dynamic_value}")
+    if new_dynamic_value > 32 and email_to:
+        print(new_dynamic_value)
+        send_mail(email_to, f': Atentie,este prea cald in casa')
+
+    elif new_dynamic_value < 19 and email_to:
+        send_mail(email_to, f': Atentie,este prea frig in casa')
+
+
+    emit('update_dynamic_value', {'dynamic_value': dynamic_value}, broadcast=True)
 
 
 @socketio.on('send_current_temperature')
 def handle_send_current_temperature():
-    print("da2")
     if email_to:
-        print("da3")
-        send_mail(email_to, f'Test email. Dynamic value: {dynamic_value}')
+        send_mail(email_to, f'Temperatura actuala: {dynamic_value}')
 
 
 if __name__ == '__main__':
